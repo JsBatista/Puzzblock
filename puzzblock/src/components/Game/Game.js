2932d0645d7720dbcf7cb8305588357fdc266d80
Joao Batista <joaobatistafreitasfilho@gmail.com>
2018-10-11 21:28:38 -0300

    Swapping functions done

--- puzzblock/src/components/Game/Game.js
+++ puzzblock/src/components/Game/Game.js
@@ -4,29 +4,42 @@ import NumBlock from './NumBlock/NumBlock';
 class Game extends Component {
 
     state = {
-        board: []
+        board: [],
+        blankIndex: 0
     }
 
     initializeBoard = () => {
         let numbers = [0,2,3,4,5,6,7,8,9];
         let newBoard = [];
+        let blankIndex = 0;
         for(let i = 0; i < 9; i++){
             let selected = Math.floor( Math.random() * numbers.length );
+            if(numbers[selected] == 0){
+                blankIndex = i
+            }
             newBoard[i] = numbers[selected];
             numbers.splice(selected, 1);
         }
         this.setState({
-            board: [...newBoard]
+            board: [...newBoard],
+            blankIndex: blankIndex
         });
     }
 
-    swapWithBlank = (number) => {
-        console.log(number);
+    swapWithBlank = (number, index) => {
+        if(index == this.state.blankIndex - 1 || index == this.state.blankIndex + 1 || index == this.state.blankIndex + 3 || index == this.state.blankIndex -3){
+            let tempState = {...this.state};
+            [tempState.board[index], tempState.board[this.state.blankIndex]] = [tempState.board[this.state.blankIndex], tempState.board[index]];
+            tempState.blankIndex = index;
+            this.setState({
+                ...tempState
+            })
+        }
     }
 
     render(){
 
-        let blocks = this.state.board.map((c)=> <NumBlock swap={this.swapWithBlank} number={c} key={c}/>);
+        let blocks = this.state.board.map((c, i)=> <NumBlock swap={this.swapWithBlank} number={c} index={i} key={c}/>);
 
         return(
             <div>
--- puzzblock/src/components/Game/NumBlock/NumBlock.js
+++ puzzblock/src/components/Game/NumBlock/NumBlock.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const numBlock = (props) => {
     return(
-        <div className="blockDiv" onClick={()=>{props.swap(props.number)}}>
+        <div className="blockDiv" onClick={()=>{props.swap(props.number, props.index)}}>
             <h1>{props.number==0? " ": props.number}</h1>
         </div>
     );
